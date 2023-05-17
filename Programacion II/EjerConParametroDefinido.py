5aaa077360ea82a009a8e9b3df4ccc87bbc6d810
IvanMarek <marekivan08@gmail.com>
2023-05-17 00:21:59 -0300

    actualizacion

--- Programacion II/EjerConParametroDefinido.py
+++ Programacion II/EjerConParametroDefinido.py
@@ -4,8 +4,41 @@ import os
 
 """os.system("cls") como usar funcion para limpiar pantalla"""
 
-def agendaSemanal():
-    mat=[]
-    diasSemana=["Lunes,Martes,Miercoles,Jueves;viernes,Sabádo,Domingo"]
-    num=0
+
+"""def listanombres(nombre= "usuario anonimo"):
     cont=0
+    producto=""
+    Productos=[]
+    while cont < 6:
+        producto= input("Ingresa un producto: ")
+        Productos.append(producto)
+        cont+=1
+    print(nombre) 
+    print(Productos)
+
+listanombres()"""
+
+
+"""def delta(a,b,c=5):
+    delta= b**2 -4 * a* c
+    print(delta)
+delta(4,-2)"""
+
+"""def listaModificable  (datos = ["nombre","dni","telefono"]):
+    salida = []
+    for i in datos:
+        salida.append(input(f"ingrese {i}:  "))
+
+    return salida
+
+print (listaModificable (["apellido","nacionalindad", ""]))"""
+ 
+
+
+
+
+
+
+
+
+
