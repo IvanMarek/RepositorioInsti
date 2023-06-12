66ebb4877f835b200f2087b47c7f30fa90dbfb4f
Ivan Marek <129914214+IvanMarek@users.noreply.github.com>
2023-06-12 11:59:06 -0300

    actu

---
+++ Programacion II/CarritoCompras/menuCarrito.py
@@ -0,0 +1,16 @@
+
+
+def menuCarro (opciones):
+
+    msjBienvenida="                     Bienvenido a compras Ivi's                      "
+    print(f"""
+
+                        {msjBienvenida}
+    
+    
+    
+    
+    """)
+    while == true:
+        if opciones ==1:
+
