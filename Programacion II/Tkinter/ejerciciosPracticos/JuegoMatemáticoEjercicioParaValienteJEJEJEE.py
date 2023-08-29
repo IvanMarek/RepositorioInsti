7cdb73a42c7982ee76df131201146fd74ee64014
IvanMarek <marekivan08@gmail.com>
2023-08-29 13:57:44 -0300

    actualizacion extra

--- Programacion II/Tkinter/ejerciciosPracticos/JuegoMatemáticoEjercicioParaValienteJEJEJEE.py
+++ Programacion II/Tkinter/ejerciciosPracticos/JuegoMatemáticoEjercicioParaValienteJEJEJEE.py
@@ -45,6 +45,7 @@ class Aplicacion():
         self.signoAzar.config(text=self.signoEs)
 
     def calcular(self):
+
         self.numero1= str(self.num1)
         self.numero2= str(self.num2)
         calculara= self.numero1 + self.signoEs + self.numero2
@@ -64,9 +65,12 @@ class Aplicacion():
             self.EtiqResultado.insert(0," Error de sintaxis ")
 
 
+
+
     def createWidgets(self):
         self.contador= 0
         self.dificult= []
+        n= IntVar()
 
         self.primerNum= ttk.Entry(self.window, width=17, justify=CENTER)
         self.primerNum.grid(row=2, column= 1, padx=(20,10), pady=(20,10), sticky=N)
@@ -79,31 +83,34 @@ class Aplicacion():
         self.segundoNum.grid(row=2, column= 3, padx=(10,20),  pady=(20,10), sticky=N)
         self.segundoNum.config(state="readonly")
 
-        self.botonNuevoJuego= ttk.Button(self.window, text= " Nuevo juego ", command= lambda: [self.primerNum.config(state=NORMAL) ,self.primerNum.delete(0,END), self.primerNum.config(state="readonly"), self.segundoNum.config(state=NORMAL) , self.segundoNum.delete(0,END), self.segundoNum.config(state="readonly"),self.numerosAlazar(), self.signosAlAzar(), self.segundoNum.delete(0,END), self.EtiqResultado.delete(0,END),self.EtiqResultado.config(state=NORMAL)])
+        self.botonNuevoJuego= ttk.Button(self.window, text= " Nuevo juego ", command= lambda: [self.primerNum.config(state=NORMAL) ,self.primerNum.delete(0,END), self.primerNum.config(state="readonly"), self.segundoNum.config(state=NORMAL) , self.segundoNum.delete(0,END), self.segundoNum.config(state="readonly"),self.numerosAlazar(), self.signosAlAzar(), self.segundoNum.delete(0,END), self.EtiqResultado.delete(0,END),self.EtiqResultado.config(state=NORMAL), self.botonNuevoJuego.config(state=DISABLED), self.botonResultado.config(state=NORMAL)])
         self.botonNuevoJuego.grid(row=3, column=4, sticky=N)
+        self.botonNuevoJuego.config(state=DISABLED)
 
 
-        self.botonSuma= ttk.Radiobutton(self.window, text=" Suma ")
+        self.botonSuma= ttk.Radiobutton(self.window, text=" Suma ", variable=n, value=1)
         self.botonSuma.grid(row=3, column=1, sticky=W+E, padx=(20,0), pady=(25,0))
         self.botonSuma.config(state=DISABLED)
 
-        self.botonRestar= ttk.Radiobutton(self.window, text=" Restar ")
+        self.botonRestar= ttk.Radiobutton(self.window, text=" Restar ", variable=n, value=2)
         self.botonRestar.grid(row=4, column=1, sticky=W+E, padx=(20,0), pady=(3,0))
         self.botonRestar.config(state=DISABLED)
 
-        self.botonMulti= ttk.Radiobutton(self.window, text=" Multiplicar ")
+        self.botonMulti= ttk.Radiobutton(self.window, text=" Multiplicar ", variable=n, value=3)
         self.botonMulti.grid(row=5, column=1, sticky=W+E, padx=(20,0), pady=(3,0))
         self.botonMulti.config(state=DISABLED)
 
-        self.botonDividir= ttk.Radiobutton(self.window, text=" Dividir ")
+        self.botonDividir= ttk.Radiobutton(self.window, text=" Dividir ", variable=n, value=4)
         self.botonDividir.grid(row=6, column=1, sticky=W+E, padx=(20,0), pady=(3,0))
         self.botonDividir.config(state=DISABLED)
 
         self.EtiqResultado= ttk.Entry(self.window, width=17, justify=CENTER)
         self.EtiqResultado.grid(row=5, column=4)
+        self.EtiqResultado.config(state="readonly")
 
-        self.botonResultado= ttk.Button(self.window, text=" Resultado ", command= lambda: [self.calcular(), self.EtiqResultado.delete(0,END), self.EtiqResultado.config(state="readonly")])
+        self.botonResultado= ttk.Button(self.window, text=" Resultado ", command= lambda: [self.calcular(), self.EtiqResultado.delete(0,END), self.EtiqResultado.config(state="readonly"), self.botonNuevoJuego.config(state=NORMAL), self.botonResultado.config(state=DISABLED)])
         self.botonResultado.grid(row=7, column=4, sticky=W+E)
+        self.botonResultado.config(state=DISABLED)
 
         self.contjuegos=0
         self.contJuegosBuenos=0
@@ -127,13 +134,13 @@ class Aplicacion():
         self.cantJuegosMalos= ttk.Label(self.window, text="0", justify=RIGHT)
         self.cantJuegosMalos.grid(row=11, column=4, padx=(5,5), pady=(5,0), sticky=W)
 
-        self.botonFacil= ttk.Radiobutton(self.window, text=" Fácil", value=1,command= lambda: [self.dificult.append(1), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
+        self.botonFacil= ttk.Radiobutton(self.window, text=" Fácil", value=1,command= lambda: [self.botonNuevoJuego.config(state=NORMAL) , self.dificult.append(1), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
         self.botonFacil.grid(row=11, column=0, sticky=W+E, padx=(10,0))
 
-        self.botonNormal= ttk.Radiobutton(self.window, text=" Normal ", value=2,command= lambda:[self.dificult.append(2), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
+        self.botonNormal= ttk.Radiobutton(self.window, text=" Normal ", value=2,command= lambda:[self.botonNuevoJuego.config(state=NORMAL), self.dificult.append(2), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
         self.botonNormal.grid(row=11, column=1, sticky=W+E, padx=(30,20))
 
-        self.botonDificil= ttk.Radiobutton(self.window, text=" Difícil ", value=3,command= lambda:[self.dificult.append(3), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
+        self.botonDificil= ttk.Radiobutton(self.window, text=" Difícil ", value=3,command= lambda:[self.botonNuevoJuego.config(state=NORMAL), self.dificult.append(3), self.dificultas(), self.cantJuegos.config(text="0"), self.cantJuegosBuenos.config(text="0"), self.cantJuegosMalos.config(text="0")])
         self.botonDificil.grid(row=11, column=2, sticky=W+E, padx=(10,0))
 
 
