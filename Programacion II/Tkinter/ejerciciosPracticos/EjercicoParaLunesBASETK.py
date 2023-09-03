1bffdc0671a4ebc19f98574972cb2b966ae9dd32
IvanMarek <marekivan08@gmail.com>
2023-09-03 20:00:42 -0300

    actualizacion

--- Programacion II/Tkinter/ejerciciosPracticos/EjercicoParaLunesBASETK.py
+++ Programacion II/Tkinter/ejerciciosPracticos/EjercicoParaLunesBASETK.py
@@ -10,7 +10,7 @@ def cargar_datos():
     tree.delete(*tree.get_children()) # Borrar datos existentes en el Treeview
     cursor = conexion.cursor()
     consulta_sql = """
-        SELECT producto.Nombre, producto.precio, marca.Nombre, categoria.Nombre
+        SELECT producto.Nombre, producto.precio, producto.stock, marca.Nombre, categoria.Nombre
         FROM producto
         INNER JOIN marca ON producto.CodMarca = marca.CodMarca
         INNER JOIN categoria ON producto.CodCategoria = categoria.CodCategoria
@@ -21,21 +21,23 @@ def cargar_datos():
 
 # Crear ventana
 root = tk.Tk()
-root.title("Consulta de Alumnos")
+root.title("Consultar productos")
 
 # Crear Treeview para mostrar la información
-tree = ttk.Treeview(root, columns=("Nombre", "Precio", "Marca", "Categoria"))
+tree = ttk.Treeview(root, columns=("Nombre", "Precio", "stock",  "Marca", "Categoria"))
 tree.heading("#1", text="Nombre", anchor=tk.CENTER)  # Centrar el encabezado 'Nombre'
 tree.heading("#2", text="Precio", anchor=tk.CENTER)  # Centrar el encabezado 'Precio'
-tree.heading("#3", text="Marca", anchor=tk.CENTER)   # Centrar el encabezado 'Marca'
-tree.heading("#4", text="Categoria", anchor=tk.CENTER)  # Centrar el encabezado 'Categoria'
+tree.heading("#3", text="Stock", anchor=tk.CENTER)   # centrar el encabezado 'stock'
+tree.heading("#4", text="Marca", anchor=tk.CENTER)   # Centrar el encabezado 'Marca'
+tree.heading("#5", text="Categoria", anchor=tk.CENTER)  # Centrar el encabezado 'Categoria'
 tree.column("#0", width=0, stretch=tk.NO)  # Ocultar la columna #0 que habitualmente muestra las primary key de los objetos
 tree.pack(padx=10, pady=10)
 
 tree.column("#1", anchor=tk.CENTER)  # Centrar datos en la columna 'Nombre'
 tree.column("#2", anchor=tk.CENTER)  # Centrar datos en la columna 'Precio'
-tree.column("#3", anchor=tk.CENTER)  # Centrar datos en la columna 'Marca'
-tree.column("#4", anchor=tk.CENTER)  # Centrar datos en la columna 'Categoria'
+tree.column("#3", anchor=tk.CENTER)  # Centrar datos en la columna 'stock'
+tree.column("#4", anchor=tk.CENTER)  # Centrar datos en la columna 'Marca'
+tree.column("#5", anchor=tk.CENTER)  # Centrar datos en la columna 'Categoria'
 tree.column("#0", width=0, stretch=tk.NO)  # Ocultar la columna #0 que habitualmente muestra las primary key de los objetos
 tree.pack(padx=10, pady=10)
 
@@ -48,3 +50,4 @@ root.mainloop()
 
 # Cerrar la conexión a la base de datos al cerrar la aplicación
 conexion.close()
+
